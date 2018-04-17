19305d2b6f014f3ce668a8004dbee5538955a777
Jonathan Williams <jtw365@live.missouristate.edu>
2018-04-17 00:28:12 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,13 +22,6 @@ class Transfer
     sender.balance = sender.balance - amount
     receiver.balance = receiver.balance + amount
     @status = "complete"
-    if status == "complete"
-      break
-    end
-
-
-    #binding.pry
-  end
 
 
 end
